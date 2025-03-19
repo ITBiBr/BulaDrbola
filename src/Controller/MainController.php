6420b78d1e69014c833830499a8cc89ed3faa7af
Ludek Strobl <strobl@biskupstvi.cz>
2025-03-19 12:04:42 +0100

    pokus o menu

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,8 +11,22 @@ final class MainController extends AbstractController
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
+        $lorem = $this->generateLoremIpsum(50);
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'lorem' => $lorem,
         ]);
+    }
+
+    private function generateLoremIpsum(int $lines): string
+    {
+        $lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.";
+        $text = '';
+
+        for ($i = 0; $i < $lines; $i++) {
+            $text .= $lorem . "<br>";
+        }
+
+        return $text;
     }
 }
