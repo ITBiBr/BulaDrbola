103a1928e6a82046a90a6b991809ac8c6e130b82
Štrobl Luděk <strobl@biskupstvi.cz>
2025-08-06 10:15:41 +0200

    materialy menu

---
+++ src/Controller/MaterialyController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MaterialyController extends AbstractController
+{
+    #[Route('/materialy', name: 'app_materialy')]
+    public function index(): Response
+    {
+        return $this->render('materialy/index.html.twig', [
+            'controller_name' => 'MaterialyController',
+        ]);
+    }
+}
