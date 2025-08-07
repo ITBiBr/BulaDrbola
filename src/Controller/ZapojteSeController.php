dc314a6b27236d5d26abe072d4e38c8c1c37130b
Štrobl Luděk <strobl@biskupstvi.cz>
2025-08-07 10:39:14 +0200

    zapojte se controller

---
+++ src/Controller/ZapojteSeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ZapojteSeController extends AbstractController
+{
+    #[Route('/zapojte/se', name: 'app_zapojte_se')]
+    public function index(): Response
+    {
+        return $this->render('zapojte_se/index.html.twig', [
+            'controller_name' => 'ZapojteSeController',
+        ]);
+    }
+}
