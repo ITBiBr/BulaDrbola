9ea897507ecddbd2c2c661879efc2226fdc7ee8f
Štrobl Luděk <strobl@biskupstvi.cz>
2025-08-07 12:46:20 +0200

    zmena route zapojte-se

--- src/Controller/ZapojteSeController.php
+++ src/Controller/ZapojteSeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ZapojteSeController extends AbstractController
 {
-    #[Route('/zapojte/se', name: 'app_zapojte_se')]
+    #[Route('/zapojte-se', name: 'app_zapojte_se')]
     public function index(): Response
     {
         return $this->render('zapojte_se/index.html.twig', [
