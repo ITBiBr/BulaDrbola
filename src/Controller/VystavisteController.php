65937b6c3c2367bd5d8b28689af9e95d66382c20
Štrobl Luděk <strobl@biskupstvi.cz>
2025-10-09 10:10:53 +0200

    clanek vystaviste je potreba renderovat bez paticky

--- src/Controller/VystavisteController.php
+++ src/Controller/VystavisteController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Clanky;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class VystavisteController extends AbstractController
 {
     #[Route('/vystaviste', name: 'app_vystaviste')]
-    public function index(): RedirectResponse
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->redirectToRoute('clanky_url', ['url' => 'vystaviste']);
+        $clanek = $entityManager->getRepository(Clanky::class)->findOneBy(['url'=>'vystaviste']);
+
+        if (!$clanek)
+            throw new NotFoundHttpException();
+        return $this->render('vystaviste/index.html.twig', [
+            'controller_name' => 'VystavisteController',
+            'clanek' => $clanek,
+            'paticka'=> false,
+        ]);
     }
 }
