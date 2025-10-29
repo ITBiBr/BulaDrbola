e8d96dfc907acaf9e7146e5d5bad448596af0cf6
Štrobl Luděk <strobl@biskupstvi.cz>
2025-10-29 08:06:43 +0100

    úprava vzhledu formuláře doladění povinných položek

--- src/Form/DobrovolniciType.php
+++ src/Form/DobrovolniciType.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Count;
 
 class DobrovolniciType extends AbstractType
 {
@@ -27,13 +28,16 @@ class DobrovolniciType extends AbstractType
                     'class' => 'form-control mb-3',
                     'placeholder' => 'Zadejte své jméno',
                 ],
+                'label_attr' => ['class' => 'fw-bold'],
             ])
             ->add('prijmeni', TextType::class, [
                 'label' => 'Příjmení',
                 'attr' => ['class' => 'form-control mb-3'],
+                'label_attr' => ['class' => 'fw-bold'],
             ])
             ->add('vek', NumberType::class, [
                 'label' => 'Věk',
+                'label_attr' => ['class' => 'fw-bold'],
                 'attr' => [
                     'class' => 'form-control mb-3',
                     'maxlength' => 3,  // max 3 znaky v inputu
@@ -41,10 +45,12 @@ class DobrovolniciType extends AbstractType
             ])
             ->add('email', EmailType::class, [
                 'label' => 'E-mail',
+                'label_attr' => ['class' => 'fw-bold'],
                 'attr' => ['class' => 'form-control mb-3'],
             ])
             ->add('telefon', TelType::class, [
                 'label' => 'Telefon',
+                'label_attr' => ['class' => 'fw-bold'],
                 'attr' => ['class' => 'form-control mb-3'],
             ])
 
@@ -54,8 +60,16 @@ class DobrovolniciType extends AbstractType
                 'multiple' => true,
                 'expanded' => true, // musí být true
                 'label' => 'Na jakých aktivitách se můžete/chcete podílet?',
-                'choice_attr' => fn($choice, $key, $value) => ['class' => 'form-check-input'],
-                'label_attr' => ['class' => 'form-check-label'],
+                'label_attr' => ['class' => 'form-check-label fw-bold mt-3'],
+                'choice_attr' => fn($choice, $key, $value) => [
+                    'class' => 'form-check-input mt-3 mb-2 me-2 d-block',
+                ],
+                'constraints' => [
+                    new Count([
+                        'min' => 1,
+                        'minMessage' => 'Vyberte alespoň jednu aktivitu.',
+                    ]),
+                ],
             ])
             ->add('isZkusenosti', ChoiceType::class, [
                 'label' => 'Máte předchozí zkušenosti s dobrovolnickou službou?',
@@ -66,7 +80,7 @@ class DobrovolniciType extends AbstractType
                 'expanded' => true,
                 'multiple' => false,
                 'choice_attr' => fn($choice, $key, $value) => ['class' => 'form-check-input'],
-                'label_attr' => ['class' => 'form-check-label'],
+                'label_attr' => ['class' => 'form-check-label fw-bold mt-3'],
             ])
 
             ->add('isSouhlasGdpr', CheckboxType::class, [
