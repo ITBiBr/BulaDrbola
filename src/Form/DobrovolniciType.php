a69e125ccf9b9476354efcd5d84bace2b2506034
Štrobl Luděk <strobl@biskupstvi.cz>
2025-10-29 09:38:37 +0100

    doplnění formuláře a úprava css

--- src/Form/DobrovolniciType.php
+++ src/Form/DobrovolniciType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Dobrovolnici;
 use App\Entity\DobrovolniciAkceCiselnik;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -12,10 +13,13 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\Constraints\Count;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class DobrovolniciType extends AbstractType
 {
@@ -32,7 +36,10 @@ class DobrovolniciType extends AbstractType
             ])
             ->add('prijmeni', TextType::class, [
                 'label' => 'Příjmení',
-                'attr' => ['class' => 'form-control mb-3'],
+                'attr' => [
+                    'class' => 'form-control mb-3',
+                    'placeholder' => 'Zadejte své příjmení',
+                    ],
                 'label_attr' => ['class' => 'fw-bold'],
             ])
             ->add('vek', NumberType::class, [
@@ -40,18 +47,25 @@ class DobrovolniciType extends AbstractType
                 'label_attr' => ['class' => 'fw-bold'],
                 'attr' => [
                     'class' => 'form-control mb-3',
+                    'placeholder' => 'Zadejte svůj věk',
                     'maxlength' => 3,  // max 3 znaky v inputu
                 ],
             ])
             ->add('email', EmailType::class, [
                 'label' => 'E-mail',
                 'label_attr' => ['class' => 'fw-bold'],
-                'attr' => ['class' => 'form-control mb-3'],
+                'attr' => [
+                    'class' => 'form-control mb-3',
+                    'placeholder' => 'Zadejte svou e-mailovou adresu',
+                    ],
             ])
             ->add('telefon', TelType::class, [
                 'label' => 'Telefon',
                 'label_attr' => ['class' => 'fw-bold'],
-                'attr' => ['class' => 'form-control mb-3'],
+                'attr' => [
+                    'class' => 'form-control mb-3',
+                    'placeholder' => 'Zadejte své telefonní číslo',
+                    ],
             ])
 
             ->add('akce', EntityType::class, [
@@ -62,7 +76,7 @@ class DobrovolniciType extends AbstractType
                 'label' => 'Na jakých aktivitách se můžete/chcete podílet?',
                 'label_attr' => ['class' => 'form-check-label fw-bold mt-3'],
                 'choice_attr' => fn($choice, $key, $value) => [
-                    'class' => 'form-check-input mt-3 mb-2 me-2 d-block',
+                    'class' => 'form-check-input mt-3 me-2 d-block',
                 ],
                 'constraints' => [
                     new Count([
@@ -70,6 +84,12 @@ class DobrovolniciType extends AbstractType
                         'minMessage' => 'Vyberte alespoň jednu aktivitu.',
                     ]),
                 ],
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('a')
+                        ->where('a.isActive = :active')
+                        ->setParameter('active', true);
+                        //->orderBy('a.poradi', 'ASC');
+                },
             ])
             ->add('isZkusenosti', ChoiceType::class, [
                 'label' => 'Máte předchozí zkušenosti s dobrovolnickou službou?',
@@ -82,7 +102,41 @@ class DobrovolniciType extends AbstractType
                 'choice_attr' => fn($choice, $key, $value) => ['class' => 'form-check-input'],
                 'label_attr' => ['class' => 'form-check-label fw-bold mt-3'],
             ])
+            ->add('zkusenosti', TextareaType::class, [
+                'label' => 'Pokud ano, jaké zkušenosti s organizací akcí/dobrovolnictvím máte?',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Popište své zkušenosti',
+                ],
+                'required' => false,
+                'label_attr' => [
+                    'class' => 'form-label fw-bold mt-3',
+                ],
+                'constraints' => [
+                    new Callback(function ($value, ExecutionContextInterface $context) {
+                        $formData = $context->getRoot()->getData(); // vrací objekt Dobrovolnici
 
+                        // pokud isZkusenosti je true a zkusenosti je prázdné
+                        if ($formData->isZkusenosti() === true && empty($value)) {
+                            $context->buildViolation('Pokud máte zkušenosti s organizací, stručně popište jaké.')
+                                ->addViolation();
+                        }
+                    }),
+                ],
+
+            ])
+            ->add('vzkaz', TextareaType::class, [
+                'label' => 'Vzkaz organizátorům',
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Napište nám své připomínky nebo dotazy…',
+                ],
+                'label_attr' => [
+                    'class' => 'form-label fw-bold mt-3',
+                ],
+
+            ])
             ->add('isSouhlasGdpr', CheckboxType::class, [
                 'label' => 'Souhlasím se zpracováním osobních údajů (GDPR)',
 
