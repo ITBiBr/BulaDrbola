8858ed20d338a226d748ab04df79dfb3231c75d2
Štrobl Luděk <strobl@biskupstvi.cz>
2025-12-08 11:32:06 +0100

    razeni podle ceske abecedy u souboru

--- src/Controller/Twig/GlobalVariables.php
+++ src/Controller/Twig/GlobalVariables.php
@@ -17,11 +17,18 @@ class GlobalVariables
         $kategorie = $this->entityManager->getRepository(MaterialyKategorie::class)->findAll();
         $kategorieData = [];
         foreach ($kategorie as $kat) {
+            $materialies = $kat->getMaterialies()->toArray();
+
+            // Seřazení podle Nazev vzestupně
+            $collator = new \Collator('cs_CZ'); // české řazení
+            usort($materialies, function($a, $b) use ($collator) {
+                return $collator->compare($a->getNazev(), $b->getNazev());
+            });
             $kategorieData[] = [
                 'id' => $kat->getId(),
                 'Kategorie' => $kat->getKategorie(),
                 'slug' => $this->slugger->slug($kat->getKategorie())->lower(),
-                'materialies' => $kat->getMaterialies(),
+                'materialies' => $materialies,
             ];
         }
         return $kategorieData;
