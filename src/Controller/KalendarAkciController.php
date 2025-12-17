e4e30ac322d118d2b4680c0ad1cd30bb2c7589fa
Štrobl Luděk <strobl@biskupstvi.cz>
2025-12-17 12:47:13 +0100

    zobrazeni konkretni akce a uprava odkazu v aktualitach

--- src/Controller/KalendarAkciController.php
+++ src/Controller/KalendarAkciController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Akce;
+use App\Entity\Aktuality;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class KalendarAkciController extends AbstractController
@@ -18,6 +20,20 @@ final class KalendarAkciController extends AbstractController
         return $this->render('kalendar_akci/index.html.twig', [
             'controller_name' => 'KalendarAkciController',
             'akce' => $akce,
+            'paticka'=> true,
+        ]);
+    }
+
+    #[Route('/akce/{url}', name: 'akce_url')]
+    public function showAktualita(string $url, EntityManagerInterface $entityManager): Response
+    {
+        $akce = $entityManager->getRepository(Akce::class)->findOneBy(['url'=>$url]);
+
+        if (!$akce)
+            throw new NotFoundHttpException();
+        return $this->render('kalendar_akci/akce.html.twig', [
+            'controller_name' => 'AktualityController',
+            'aktualita' => $akce,
             'paticka'=> true,
         ]);
     }
