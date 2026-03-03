e8235e7d14bdeeff59d154ae179e85b9f68047be
Štrobl Luděk <strobl@biskupstvi.cz>
2026-03-03 12:31:48 +0100

    clanky reaguji odkazy na kotvy

--- src/Controller/ClankyController.php
+++ src/Controller/ClankyController.php
@@ -12,20 +12,45 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
-final class ClankyController extends AbstractController
+class ClankyController extends AbstractController
 {
+    public function __construct(private readonly SluggerInterface $slugger)
+    {
+    }
     #[Route('/clanky/{url}', name: 'clanky_url')]
-    public function showAktualita(string $url, EntityManagerInterface $entityManager): Response
+    public function showClanek(string $url, EntityManagerInterface $entityManager): Response
     {
         $clanek = $entityManager->getRepository(Clanky::class)->findOneBy(['url'=>$url]);
 
         if (!$clanek)
             throw new NotFoundHttpException();
+        // Přepracování obsahu článku a získání H1 nadpisů
+        [$processedContent, $headings] = $this->addHeadingsId($clanek->getObsah());
+        dump($processedContent);
         return $this->render('clanky/clanek.html.twig', [
             'controller_name' => 'ClankyController',
             'clanek' => $clanek,
+            'obsah' => $processedContent,
+            'headings' => $headings,
             'paticka'=> true,
         ]);
+
+
+
+    }
+    private function addHeadingsId(string $text): array
+    {
+        $headings = [];
+
+        $processedText = preg_replace_callback('/<h1>([^<]+)<\/h1>/ui', function ($matches) use (&$headings) {
+            $heading = trim($matches[1]);
+            $id = strtolower($this->slugger->slug($heading));
+            $headings[] = ['id' => $id, 'title' => $heading];
+            return sprintf('<h5 class="scroll-offset text-primary fw-bold" id="%s">%s</h5>', $id, $heading);
+        }, $text);
+
+        return [$processedText, $headings];
     }
 }
--- src/Controller/VystavisteController.php
+++ src/Controller/VystavisteController.php
@@ -9,19 +9,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class VystavisteController extends AbstractController
+final class VystavisteController extends ClankyController
 {
     #[Route('/vystaviste', name: 'app_vystaviste')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function vystaviste(EntityManagerInterface $entityManager): Response
     {
-        $clanek = $entityManager->getRepository(Clanky::class)->findOneBy(['url'=>'vystaviste']);
-
-        if (!$clanek)
-            throw new NotFoundHttpException();
-        return $this->render('vystaviste/index.html.twig', [
-            'controller_name' => 'VystavisteController',
-            'clanek' => $clanek,
-            'paticka'=> false,
-        ]);
+        // Přesměrování na showAktualita s URL 'vystaviste'
+        return $this->showClanek('vystaviste', $entityManager);
     }
 }
