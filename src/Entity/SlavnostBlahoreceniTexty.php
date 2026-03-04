9c48f1939a6d0c7d1940f2374371938f6db58925
Štrobl Luděk <strobl@biskupstvi.cz>
2026-03-04 14:27:20 +0100

    acoordion pro vsechny casti stranky

--- src/Entity/SlavnostBlahoreceniTexty.php
+++ src/Entity/SlavnostBlahoreceniTexty.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\SlavnostBlahoreceniTextyRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SlavnostBlahoreceniTextyRepository::class)]
@@ -16,7 +17,7 @@ class SlavnostBlahoreceniTexty
     #[ORM\Column(length: 255)]
     private ?string $Titulek = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: Types::TEXT)]
     private ?string $Text = null;
 
     #[ORM\ManyToOne(inversedBy: 'slavnostBlahoreceniTexties')]
