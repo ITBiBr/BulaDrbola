4b1060df212b6c4bcc65131ec206bd0d9af127eb
Štrobl Luděk <strobl@biskupstvi.cz>
2026-03-04 13:14:57 +0100

    zobrazení textu programu

--- src/Controller/SlavnostBlahoreceniController.php
+++ src/Controller/SlavnostBlahoreceniController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\SlavnostBlahoreceniKategorie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,11 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SlavnostBlahoreceniController extends AbstractController
 {
     #[Route('/slavnost-blahoreceni', name: 'app_slavnost_blahoreceni')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $kategorieTextu = $entityManager->getRepository(SlavnostBlahoreceniKategorie::class)->findAll();
         return $this->render('slavnost_blahoreceni/index.html.twig', [
-
-            'paticka' => false
+            'kategorieTextu' => $kategorieTextu,
+            'paticka' => false,
         ]);
     }
 }
