8de9e60a5b2d3c4c63117035d0e39dd184f68a70
Štrobl Luděk <strobl@biskupstvi.cz>
2026-03-07 19:50:59 +0100

    presmerovani stranky vystaviste na slavnost-blahoreceni

--- src/Controller/VystavisteController.php
+++ src/Controller/VystavisteController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Clanky;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,9 +14,20 @@ use Symfony\Component\Routing\Attribute\Route;
 final class VystavisteController extends ClankyController
 {
     #[Route('/vystaviste', name: 'app_vystaviste')]
-    public function vystaviste(EntityManagerInterface $entityManager): Response
+    public function vystaviste(EntityManagerInterface $entityManager, Security $security): Response
     {
-        // Přesměrování na showAktualita s URL 'vystaviste'
-        return $this->showClanek('vystaviste', $entityManager);
+        $datumZverejneni = new \DateTimeImmutable(
+            $this->getParameter('datum_zverejneni')
+        );
+
+        $now = new \DateTimeImmutable();
+
+        if ($now < $datumZverejneni  && !$security->isGranted('IS_AUTHENTICATED_FULLY')) { //muze se zverejnit nebo není user prihlasen
+            // Přesměrování na showAktualita s URL 'vystaviste'
+            return $this->showClanek('vystaviste', $entityManager);
+        }
+
+        return new RedirectResponse('slavnost-blahoreceni');
+
     }
 }
